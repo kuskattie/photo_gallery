87166c528e5b2a6b394a12aa3b09b0745240acdf
Ekaterina Danshova <ekaterinadanshova@ekaterinas-macbook-pro.home>
2025-06-30 12:15:12 +0200

    vite congif change

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/photo_gallery/' : '/',
+  base: '/photo_gallery/',
   plugins: [react()],
 })
